dcf56a83baeaf2143454391f6669c14c426164df
rciupek <rudekreg@gmail.com>
2024-12-18 09:21:31 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="max-h-full flex flex-col overflow-auto bg-background min-h-[100vh] flex-grow">
+  </div>
+</template>
---
+++ components/navbar/TopNavbar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex !min-h-16 items-center justify-center border-b-2 border-border">
+</template>
